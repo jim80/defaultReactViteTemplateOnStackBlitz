165bba95c19b07c206678ddf729f63e2199ea9e7
jim80 <hayes.jim@gmail.com>
2024-10-03 00:30:32 +0100

    formatting

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,18 @@ import useCounter from './hooks/useCounter'
 
 function App() {
 
-  const {count : counterCount, add: counterAdd} = useCounter(42);
+  const { count: counterCount, add: counterAdd } = useCounter(42);
 
   return (
     <>
       <RandomDog></RandomDog>
       <div className="card">
-      
-      <button onClick={() => counterAdd()}>
+
+        <button onClick={() => counterAdd()}>
           count is {counterCount}
         </button>
       </div>
-     
+
 
       <p className="read-the-docs">
         some styled text
