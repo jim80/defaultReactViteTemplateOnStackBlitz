5779b9357d6df7631577353f6f2d7854e7331871
jim80 <hayes.jim@gmail.com>
2024-10-03 01:51:06 +0100

    Job Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,14 @@
 
 import './App.css';
 import RandomDog from './components/RandomDog'
-import useCounter from './hooks/useCounter'
 
 function App() {
-
-  const { count: counterCount, add: counterAdd } = useCounter(42);
 
   return (
     <>
       <RandomDog></RandomDog>
       <div className="card">
-
-        <button onClick={() => counterAdd()}>
-          count is {counterCount}
-        </button>
       </div>
-
-
-      <p className="read-the-docs">
-        some styled text
-      </p>
     </>
   );
 }
--- src/components/RandomDog.jsx
+++ src/components/RandomDog.jsx
@@ -2,41 +2,38 @@ import useFetch from "react-fetch-hook";
 import ImageViewer from './ImageViewer'
 import Loading from './Loading'
 import Error from './Error'
+import useCounter from '../hooks/useCounter'
 
 const RandomDog = () => {
 
-    const RANDOM_DOG_URL = "https://dog.ceo/api/breeds/image/random";
+  const RANDOM_DOG_URL = "https://dog.ceo/api/breeds/image/random";
 
-    const { isLoading, data, error } = useFetch(RANDOM_DOG_URL);
+  const { count: counterCount, add: counterAdd } = useCounter(1);
 
-    let getMainContent = () => {
-      if (error) {
-       return  <Error error= {error} />
+  const { isLoading, data, error } = useFetch(
+    RANDOM_DOG_URL,{ depends: [counterCount] }
+  );
+
+  let getMainContent = () => {
+    if (error) {
+      return <Error error={error} />
+    } else {
+      if (isLoading) {
+        return <Loading />
       }
-      else
-      {
-        if (isLoading){
-          return <Loading />
-        }
-        else{
-          return <ImageViewer srcUrl = {data.message} />
-        }
-
+      else {
+        return <ImageViewer srcUrl={data.message} />
       }
     }
+  }
 
-    return (
-        <>
-
-        <button onClick={() => console.log("REFRESH DOG")}>
-          Get another random dog
-        </button>
-
-        {getMainContent()}
-        
-        {/* {isLoading ? <h1>"LOADING"</h1> : <ImageViewer srcUrl = {data.message}></ImageViewer>} */}
-            
-        </>
-    )
+  return (
+    <>
+      <button onClick={() => counterAdd()}>
+        "Get another random dog"
+      </button>
+      {getMainContent()}
+    </>
+  )
 }
 export default RandomDog
