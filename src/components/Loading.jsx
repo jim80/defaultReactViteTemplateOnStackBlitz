4740dca6655c64a77f905d26295b513d9d9d47e5
ng-jim <j.hayes@nittygritty.net>
2024-10-07 01:59:57 +0100

    Added a loading animation

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,10 +1,36 @@
+/* eslint-disable react/no-unknown-property */
 import "../App.css";
 
 const Loading = () => {
   return (
     <>
       <div data-testid="loadingComponent" className="loading-component">
-        <h1>{"LOADING ...."}</h1>
+        <svg
+          className="loader"
+          viewBox="0 0 120 120"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle
+            cx="60"
+            cy="60"
+            r="50"
+            fill="none"
+            strokeWidth="8"
+            stroke="#3498db"
+          />
+          <circle
+            cx="60"
+            cy="60"
+            r="40"
+            fill="none"
+            strokeWidth="8"
+            stroke="#3498db"
+            strokeDasharray="240"
+            strokeDashoffset="0"
+            animation="spin 2s linear infinite"
+          />
+        </svg>
+        <p>Loading...</p>
       </div>
     </>
   );
